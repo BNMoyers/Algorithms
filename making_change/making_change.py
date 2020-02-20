d473b90bee4941a70d9c294ecd86696dd0c974c5
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-02-20 14:07:26 -0600

    finished with making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,8 +2,31 @@
 
 import sys
 
+# should work a lot like eating cookies, except instead of 3 we're working with 5, and the values of each option are not sequential
+# will need to figure out if the amount is higher than a given denomination; we can safely ignore all higher denominations for that number
+# definitely need a cache 
+
+
 def making_change(amount, denominations):
-  pass 
+  cache = [0 for i in range(amount +1)]
+  cache[0] = 1
+  
+  if amount < 0:
+   return 0
+  
+  if amount == 0 or amount == 1:
+    return 1
+  
+  elif cache[amount] > 0:
+    return cache[amount]
+  
+  else:
+    for coin in denominations:
+      for biggest in range(coin, amount +1):
+        difference = biggest - coin
+        if difference >=0:
+          cache[biggest] += cache[difference]
+    return cache[amount]
 
 
 if __name__ == "__main__":
